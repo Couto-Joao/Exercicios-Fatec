7abc75cad7c1323b14202a30c2bf4473feab8b7f
João Pedro Gonçalves Couto <rustincouto@gmail.com>
2025-02-25 09:02:19 -0300

    adicionando subtração do ex. 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 function seguir() {
     alert('Você agora está seguindo João Pedro');
 }
+
+function exe1() {
+    let numero1 = document.getElementById('numero1').value;
+    let numero2 = document.getElementById('numero2').value;
+    
+    let sub = numero1 - numero2;
+    
+    document.getElementById('subtracao').textContent = `O resultado é: ${sub}`;
+
+}
